5494ada3d13059bc1eacf685b45c129af389c4d7
demoks <demokstar@gmail.com>
2015-06-24 15:23:02 +0700

    update

---
+++ test.js
@@ -0,0 +1,28 @@
+﻿var dateFormat = require('dateformat'),
+	banned = require('./lib/banned')();
+	
+banned.addBann({
+	type: 'ip',
+	val: '123.456.78.90',
+	time: 1200,
+	mid: 65,
+	msg: "Bad boy"
+}, function(err){
+	if (err) console.log(err);
+});
+
+banned.inBann({
+	userID: 315,
+	ip: '123.456.78.90'
+}, function(err, res){
+	if (err) console.log(err);
+	console.log(res);
+});
+
+banned.historyBann({
+	type: 'ip',
+	val: '123.456.78.90'
+}, function(err, res){
+	if (err) console.log(err);
+	console.log(res);
+});
